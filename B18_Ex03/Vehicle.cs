5ca82b4a87407ac0bf1340c0f154bc5d2df4f121
Diamant <ndiamant@paypal.com>
2018-05-10 23:54:19 +0300

    add vehicle and wheel classes

---
+++ B18_Ex03/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+namespace B18_Ex03
+{
+    public class Vehicle
+    {
+        string m_ModelName;
+        string m_LicensingNumber;
+        float m_LeftEnergy;
+        List<Wheel> m_Wheels;
+
+        public Vehicle()
+        {
+        }
+    }
+}
---
+++ B18_Ex03/Wheel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace B18_Ex03
+{
+    public class Wheel
+    {
+        string m_ManufacturerName;
+        float m_CurrAirpressure;
+        float m_MaxAirPressure;
+
+        public Wheel(string i_ManufacturerName, float i_CurrAirpressure, float i_MaxAirPressure)
+        {
+            m_ManufacturerName = i_ManufacturerName;
+            m_CurrAirpressure = i_CurrAirpressure;
+            m_MaxAirPressure = i_MaxAirPressure;
+        }
+
+        public void addAirToWheel(float i_AirToAdd)
+        {
+            if(m_CurrAirpressure + i_AirToAdd > m_MaxAirPressure)
+            {
+                m_CurrAirpressure = m_MaxAirPressure;
+            }
+            else
+            {
+                m_CurrAirpressure += i_AirToAdd;
+            }
+        }
+    }
+}
